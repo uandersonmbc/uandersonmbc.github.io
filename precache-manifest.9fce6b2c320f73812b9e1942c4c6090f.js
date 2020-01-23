265bb7517ecadab3c5e6072ab570160699c9035c
Uanderson Nunes de Lima <uandersonmbc@hotmail.com>
2020-01-23 03:00:01 -0300

    Commit Inicial

---
+++ precache-manifest.9fce6b2c320f73812b9e1942c4c6090f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450adb17fd82b19ac26637aea32f4d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9392e078709db09cf7",
+    "url": "/static/js/2.33ec8208.chunk.js"
+  },
+  {
+    "revision": "b446d5d521c3431be3ce",
+    "url": "/static/js/main.8e74d26a.chunk.js"
+  },
+  {
+    "revision": "e369b7cc7616fd22cc4b",
+    "url": "/static/js/runtime-main.97babbd7.js"
+  }
+]);
